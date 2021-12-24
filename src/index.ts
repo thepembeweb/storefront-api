7b2315ae70d5ae16ff326ce88cf52569ec7a7caa
Pemberai Sweto <pembeweb@yahoo.com>
2021-12-24 14:16:12 +0200

    feat: add project routes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import dashboardRoutes from './handlers/dashboard';
+import orderRoutes from './handlers/orders';
+import productRoutes from './handlers/products';
+import userRoutes from './handlers/users';
 
 dotenv.config();
 
@@ -15,6 +19,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req: Request, res: Response) => {
   res.send('<h1>Storefront API</h1>');
 });
+
+dashboardRoutes(app);
+orderRoutes(app);
+productRoutes(app);
+userRoutes(app);
 
 app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
 
